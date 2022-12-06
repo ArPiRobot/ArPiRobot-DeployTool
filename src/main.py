35bfe014cf6d439d527bc09e013954a0a199b2ba
Marcus Behel <marcus.behel@mindspring.com>
2022-12-06 12:30:37 -0500

    Experimental dark mode support for windows
    - Enabled QT 6.4's experimental dark mode for windows
    - Dark mode is only enabled if correct reg key set
    - Dark mode forces fusion theme b/c windows theme looks bad right now

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 
 import sys
+import platform
 
 from PySide6.QtWidgets import QApplication, QStyleFactory
 from PySide6.QtCore import Qt, QFile, QIODevice
@@ -7,8 +8,6 @@ from PySide6.QtCore import Qt, QFile, QIODevice
 from deploy_tool import DeployToolWindow
 from util import theme_manager, settings_manager
 
-QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
-QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps)
 QApplication.setAttribute(Qt.AA_DontUseNativeMenuBar)
 
 # TODO: Stdout and Stderr redirect to log file (along with log data shown in DS log window)
@@ -19,7 +18,34 @@ try:
 except AttributeError:
     pass
 
-app = QApplication(sys.argv)
+app = None
+
+# QT 6.4 introduced experimental support for dark mode on windows
+# Note that the windows them is terrible for now, but Fusion looks good
+if platform.system() == "Windows":
+    import winreg
+    path = winreg.HKEY_CURRENT_USER
+    try:
+        key = winreg.OpenKeyEx(path, r"Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize")
+        value = winreg.QueryValueEx(key, r"AppsUseLightTheme")
+        if value[0] == 0:
+            # TODO: When QT 6.5 released, there should be better support for this
+            #       Probably a new dark / light mode API
+            #       Also windowsvista theme should support dark mode
+            #       Will likely also be auto detected / applied
+            #       In other words, all of this is probably removed with QT 6.5
+            sys.argv += ['-platform', 'windows:darkmode=2']
+            app = QApplication(sys.argv)
+            app.setStyle("Fusion")
+        if key:
+            winreg.CloseKey(key)
+    except:
+        pass
+
+# Nothing special for app creation needed
+if app is None:
+    app = QApplication(sys.argv)
+
 theme_manager.set_app(app)
 theme_manager.apply_theme(settings_manager.theme, settings_manager.larger_fonts)
 
