6fc0c663546621d8c6be10027592138fc6b58a85
Marcus Behel <marcus.behel@mindspring.com>
2022-12-09 17:06:17 -0500

    Fix issues on wayland gnome
    - Fix font scaling ignored
    - Fixed cursor size ignored
    - Fixed cursor theme ignored
    - CSD still look like fusion (no good way to fix until QT uses libdecor or wraps GTK itself)

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
 
 import sys
 import platform
+import multiprocessing
+import os
+import subprocess
+
 
 from PySide6.QtWidgets import QApplication, QStyleFactory
-from PySide6.QtCore import Qt, QFile, QIODevice
+from PySide6.QtGui import QGuiApplication
+from PySide6.QtCore import Qt, QFile, QIODevice, QEventLoop
 
 from deploy_tool import DeployToolWindow
 from util import theme_manager, settings_manager
@@ -42,7 +47,35 @@ if platform.system() == "Windows":
     except:
         pass
 
-# Nothing special for app creation needed
+# Fix gnome wayland things
+if platform.system() == "Linux":
+    # QGuiApplication.platformName() is empty until app instantiated
+    # So need to create app first, but need to change things before app create
+    # Logical solution is to create app, read platform, then destroy app
+    # But this is impossible with PySide...
+    # And multiple apps is not allowed
+    # So instead, use a subprocess. Killing subprocess kills it's app
+    def mp_target(argv, return_dict):
+        tmp = QApplication(argv)
+        return_dict['plat_name'] = QGuiApplication.platformName()
+        tmp.quit()
+    manager = multiprocessing.Manager()
+    return_dict = manager.dict()
+    p = multiprocessing.Process(target=mp_target, daemon=True, args=(sys.argv, return_dict))
+    p.start()
+    p.join()
+    plat_name = return_dict['plat_name']
+    if plat_name == "wayland" and os.environ['XDG_CURRENT_DESKTOP'].find("GNOME") != -1:
+        # Running with wayland platform plugin in a gnome session
+        cursor_size = int(subprocess.check_output(["gsettings", "get", "org.gnome.desktop.interface", "cursor-size"]))
+        text_scale_factor = float(subprocess.check_output(["gsettings", "get", "org.gnome.desktop.interface", "text-scaling-factor"]))
+        cursor_theme = subprocess.check_output(["gsettings", "get", "org.gnome.desktop.interface", "cursor-theme"]).decode()
+        os.environ['QT_FONT_DPI'] = str(int(text_scale_factor * 96))
+        os.environ['XCURSOR_SIZE'] = str(cursor_size)
+        os.environ['XCURSOR_THEME'] = cursor_theme[1:-2]
+
+
+# Nothing special for app creation needed. Create as usual.
 if app is None:
     app = QApplication(sys.argv)
 
